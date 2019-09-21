306f7539e09b806ddd0e851c258548893f1be558
sergey verevkin <sergeyverevkin@gmail.com>
2019-09-21 13:05:58 +0700

    initial commit

---
+++ pi183_20190914_Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace pi183_20190914_Console
+{
+  class Program
+  {
+    static void Main(String[] args)
+    {
+      h_PrintString();
+      // string sString = String.Empty;
+      Console.ReadKey();
+    }
+
+    private static void h_PrintString()
+    {
+      // ...
+      /*
+       ...
+       ...
+       */
+      int i1 = 1;
+      string sT = "Gdsf";
+      char ch = 'F';
+      DateTime dt = DateTime.Now;
+      DateTime dt2 = dt.AddYears(1);
+
+
+      Int32 iMax = 5;
+      iMax = iMax + 1;
+      Console.WriteLine("Печать произвольной строки. Нажмите любую клавишу");
+    }
+  }
+}
